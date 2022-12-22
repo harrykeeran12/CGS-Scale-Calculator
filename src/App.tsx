88210b21acf480f9bd8066021bf1bc008b3b3732
Harry <68599167+harrykeeran12@users.noreply.github.com>
2022-12-22 20:24:14 +0000

    fix: added all the functions back into the file. works completely now!

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
 import './App.css';
 import { useState } from 'react';
 interface IFormData {
-  assignments: Number[],
-  weighting: Number[]
+  assignments: number[],
+  weighting: number[]
+}
+interface IOutputData{
+  totalNumber : number,
+  totalCGSGrade: string | undefined
 }
 export default function App() {
 
   const [FormData, setFormData] = useState<IFormData>({ "assignments": [], "weighting": [] });
+  const [OverallGrades, setOverallGrades] = useState<IOutputData>({"totalNumber" : 0, "totalCGSGrade" : ''})
   
+  function calculateTotalGrade(newFormData: IFormData) {
+    let n;
+    newFormData.assignments.length === newFormData.weighting.length ? n = newFormData.assignments.length : n = 0;
+    let total = 0;
+
+    for (let i = 0; i < n; i++) {
+      total += newFormData.assignments[i] * newFormData.weighting[i];
+    }
+    return Math.floor(total);
+  }
+
+  function mapGrade(gradeNumber : number){
+    const CGSMap = new Map(Object.entries({
+      0: 'G3', 1: 'G2', 2: 'G1', 3: 'F3', 4: 'F2', 5: 'F1', 6: 'E3', 7: 'E2', 8: 'E1', 9: 'D3', 10: 'D2', 11: 'D1', 12: 'C3', 13: 'C2', 14: 'C1', 15: 'B3', 16: 'B2', 17: 'B1', 18: 'A5', 19: 'A4', 20: 'A3', 21: 'A2', 22: 'A1'
+    }))
+    if (CGSMap.get(gradeNumber.toString()) != undefined ){
+      setOverallGrades({ "totalNumber": gradeNumber, "totalCGSGrade": CGSMap.get(gradeNumber.toString())});
+      console.log(OverallGrades);
+      return { "totalNumber": gradeNumber, "totalCGSGrade": CGSMap.get(gradeNumber.toString()) }
+    }
+   
+    
+  }
+
+
   return (
     <main>
       <p className='display-3 mb-5'>CGS Calculator</p>
-      <div className='row p-3'>
+      <div className='v-stack p-3'>
         <div className='col'>
           <form className='h-25 d-flex flex-column gap-2 container-fluid justify-content-center w-50'>
             <div className="mb-3">
@@ -31,8 +61,16 @@ export default function App() {
                 }
               }}></input>
             </div>
-            <button type="button" className="btn btn-primary" onClick={() => console.log(FormData)}>Find grade</button>
+            <button type="button" className="btn btn-primary" onClick={() => {
+            console.log(calculateTotalGrade(FormData));
+              console.log(mapGrade(calculateTotalGrade(FormData)))
+            }}>Find grade</button>
           </form>
+        </div>
+        <div className='col'>
+          <h1>Your overall grade was a <br></br>
+          {OverallGrades.totalCGSGrade} = {OverallGrades.totalNumber}
+          </h1>
         </div>
       </div>
     </main >
